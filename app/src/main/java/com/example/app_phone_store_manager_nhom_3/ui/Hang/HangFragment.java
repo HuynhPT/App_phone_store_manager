cd67c5630ba356555395b69250fd26756fa754d8
huynh113 <huynhddph12966@fpt.edu.vn>
2021-07-21 22:19:01 +0700

    memu_option

--- app/src/main/java/com/example/app_phone_store_manager_nhom_3/ui/Hang/HangFragment.java
+++ app/src/main/java/com/example/app_phone_store_manager_nhom_3/ui/Hang/HangFragment.java
@@ -2,13 +2,19 @@ package com.example.app_phone_store_manager_nhom_3.ui.Hang;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.app_phone_store_manager_nhom_3.R;
+
+import org.jetbrains.annotations.NotNull;
 
 
 public class HangFragment extends Fragment {
@@ -17,11 +23,18 @@ public class HangFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_hang, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_add_hang, menu);
     }
 }
