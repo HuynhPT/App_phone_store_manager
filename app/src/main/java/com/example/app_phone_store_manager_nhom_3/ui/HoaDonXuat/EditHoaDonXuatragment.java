8844aa5f1de5a8c3078f465fd75b5d8225f37099
NhuY86 <ytdnph12917@fpt.edu.vn>
2021-07-26 22:54:24 +0700

    HoaDonNhapXuat

---
+++ app/src/main/java/com/example/app_phone_store_manager_nhom_3/ui/HoaDonXuat/EditHoaDonXuatragment.java
@@ -0,0 +1,74 @@
+package com.example.app_phone_store_manager_nhom_3.ui.HoaDonXuat;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.app_phone_store_manager_nhom_3.R;
+
+import org.jetbrains.annotations.NotNull;
+
+
+public class EditHoaDonXuatragment extends Fragment {
+    private AppCompatActivity appCompatActivity;
+    private Drawable drawable;
+    private NavController navController;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_hoa_don_xuat, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        appCompatActivity = (AppCompatActivity) getActivity();
+        drawable = appCompatActivity.getDrawable(R.drawable.ic_backspace);
+        appCompatActivity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        appCompatActivity.getSupportActionBar().setHomeAsUpIndicator(drawable);
+        appCompatActivity.getSupportActionBar().setTitle("Cập nhập Hoá Đơn Xuất");
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_save,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull @NotNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                navController.navigate(R.id.editHDXuat_to_chiTietHDXuat);
+                return true;
+            case R.id.menu_reset:
+                return true;
+            case R.id.menu_save:
+                navController.navigate(R.id.editHDXuat_to_listHDXuat);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
